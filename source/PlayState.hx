09cd6d5b1ba56d0cf74a9314be1d842ec5f785eb
Nightskgaming <96026665+Nightskgaming@users.noreply.github.com>
2021-12-15 15:27:12 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -903,6 +903,14 @@ class PlayState extends MusicBeatState
         
         var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by Nightskgaming "), 24);
         creditTxt.scrollFactor.set();
+        creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        creditTxt.borderColor = FlxColor.BLACK;
+        creditTxt.borderSize = 3;
+        creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
+        add(creditTxt);
+        
+        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by Nightskgaming "), 24);
+        creditTxt.scrollFactor.set();
         creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxText
         
 		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
